d185c583a5f7b933699c59192e6725bf9a6dde19
Richard-Harris1992 <104505323+Richard-Harris1992@users.noreply.github.com>
2023-01-12 15:19:27 -0600

    components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import "./App.css"
+import Form from "./components/Form"
+import MovieDisplay from "./components/MovieDisplay"
+export default function App() {
+  const apiKey = "e69dfe9d"
+  //state to hold movie data
+  const [movie, setMovie] = useState(null)
+ 
+  const getList = async (searchTerm) => {
+    // make fetch request and store response
+    try {
+      const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`)
+      // Parse JSON response into a javascript object
+      const data = await response.json()
+    
+      //set the Movie state to the movie
+      setMovie(data)
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form moviesearch={getList} />
+      <MovieDisplay movie={movie} />
     </div>
-  );
+  )
 }
-
-export default App;
